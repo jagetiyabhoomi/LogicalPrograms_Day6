03548977e3909ccee3bb199b56b5b466d44536ae
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-09 18:38:55 +0530

    Find the perfect number

---
+++ PrimeNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public static class PrimeNumber
+    {
+        public static void CheckPrime()
+        {
+            int n, i, m = 0, flag = 0;
+            Console.Write("Enter the Number to check Prime: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            m = n / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write("Number is not Prime");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+                Console.Write("Number is Prime");
+        }
+    }
+    }
+
