ed5b783294ca348602eab1f18ffbbbef1d77c2cb
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-09 18:34:34 +0530

    Calling Fibonacci series method

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+    namespace LogicalPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FibonacciSeries.DisplayFibonacciSeries();
+            //PerfectNumber.CheckPerfectNumber();
+            //ReverseNumber.ReverseTheNumber();
+            //PrimeNumber.CheckPrime();
+            //CouponNumbers.GenerateCouponNumber();
+        }
+    }
+}
