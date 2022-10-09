210a95e3d3a7b3b376cdb1bae9ae0c537af6f4c6
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-09 18:36:21 +0530

    Find the Perfect Number

---
+++ PerfectNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    {
+        public static void CheckPerfectNumber()
+        {
+            int n, i, sum;
+            int mn, mx;
+            Console.WriteLine("Find perfect numbers within a given range");
+            Console.WriteLine("Input the starting number");
+            mn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the Ending number");
+            mx = Convert.ToInt32(Console.ReadLine());
+            for(n = mn; n <= mx; n++)
+            {
+                i = 1;
+                sum = 0;
+                while(i<n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
+                    }               
+                if (sum == n)
+                    Console.WriteLine("{0} ", n);
+            }
+        }
+    }
+}
